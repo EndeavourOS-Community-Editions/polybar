df345dd673d1d3b38d343551772469e1976859bf
RedBlizard <108489214+RedBlizard@users.noreply.github.com>
2023-01-15 08:34:26 +0100

    Update cpu-temp.sh
    RedNordAlternative script update

--- RedNordAlternative/scripts/cpu-temp.sh
+++ RedNordAlternative/scripts/cpu-temp.sh
@@ -3,8 +3,10 @@
 
 temp=$(sensors | grep "Tctl" | sed "s/Tctl: *+//;s/°C *//")
 if [ 1 -eq "$(echo "$temp > 80" | bc)" ]; then
-    printf "%%{F#BF616A}"
+    printf "%%{F#BF616A}🔥 "
 elif [ 1 -eq "$(echo "$temp > 60" | bc)" ]; then
-    printf "%%{F#858383}";
+    printf "%%{F#858383}🔥 ";
+elif [ 1 -eq "$(echo "$temp > 40" | bc)" ]; then
+    printf "%%{F#B48EAD}🔥 ";    
 fi
 echo "$temp"°C
